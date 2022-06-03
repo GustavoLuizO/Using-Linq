206ece79a80ee19fd3e073535f0e936485544396
Gustavo Luiz <glogustavo4321@gmail.com>
2022-06-03 11:40:12 -0300

    Take and Skip
    Program que retorna os tres primeiros valores do IEnumerable(Take), no entanto, pula os dois primeiros(SKIP)

---
+++ Using-Linq/Extract multiple elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extract_multiple_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> inteiros = new List<int> { 1,2,3,4,5,6,7,8};
+            SkipTake1.GetThirdFourthFifthItems(inteiros);
+        }
+
+        public static class SkipTake1
+        {
+            // Return the 3rd, 4th, and 5th items of the provided sequence.
+            public static IEnumerable<int> GetThirdFourthFifthItems(IEnumerable<int> inteiros)
+            {
+                return inteiros.Skip(2).Take(3);
+            }
+        }
+    }
+}
