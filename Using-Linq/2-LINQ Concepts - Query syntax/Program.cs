cd75534f56fece1078448d7c03c6b45d5bc82bf5
Gustavo Luiz <glogustavo4321@gmail.com>
2022-06-03 09:23:00 -0300

    Linq-Concepts-Query-Sintax
    Usando o Constains para trazer values onde a palavra "teste" está dentro de um Ienumerable

---
+++ Using-Linq/2-LINQ Concepts - Query syntax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2_LINQ_Concepts___Query_syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> invalues = new List<string> { "gibteste", "fateste", "heron", "ibexron", "jackalope" };
+            IEnumerable<string> result = QuerySyntax1.FilterAndSort(invalues, "teste");
+            foreach (string value in result)
+                Console.WriteLine(value);
+        }
+
+        public static class QuerySyntax1
+        {
+            public static IEnumerable<string> FilterAndSort(IEnumerable<string> inValues, string pattern)
+            {
+                return from value in inValues
+                       where value.Contains(pattern)
+                       orderby value
+                       select value;
+            }
+        }
+    }
+}
