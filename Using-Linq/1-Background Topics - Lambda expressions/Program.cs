0a519efd51ce5837f1deec6eb5305847e5620b0a
Gustavo Luiz <glogustavo4321@gmail.com>
2022-06-03 08:56:32 -0300

    1-Background-Topics-Lambda-Empression
    Using lambda expressions in Linq

---
+++ Using-Linq/1-Background Topics - Lambda expressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_Background_Topics___Lambda_expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = LambdaExpressions1.GetNextNumber(3);
+            Console.WriteLine(result);
+        }
+
+        public static class LambdaExpressions1
+        {
+            public static Func<int, int> GetNextNumber = num => num + 1;
+            //Expressão static cuja função recebe um int e retorna um int. 
+        }
+    }
+}
