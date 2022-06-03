5b3e31e1bf694a10a55f028c1496d29ed69300ed
Gustavo Luiz <glogustavo4321@gmail.com>
2022-06-03 11:58:45 -0300

    Using TakeAndSkip(While)

---
+++ Using-Linq/5-TakeAndSkip(WHILE)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5_TakeAndSkip_WHILE_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "One", "start", "more", "end", "thing" };
+            List<string> result = Count1.GetStartThroughEnd(list);
+            foreach (string item in list)
+                Console.WriteLine(item);
+        }
+
+        public static class Count1
+        {
+            // Return all words in the sequence between "start" (inclusive)
+            // and "end" (non-inclusive)
+            // For example, if given { "One", "start", "more", "end", "thing" } ...
+            // this method should return { "start", "more" }
+            public static List<string> GetStartThroughEnd(List<string> words)
+            {
+                return words.TakeWhile(x => x!= "start").SkipWhile(x => x != "end").ToList();
+            }
+        }
+    }
+}
